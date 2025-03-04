f866b5e21e3b9acaee2ee4d8d0d588143186e3fa
Tania <bhardwajtania8438@gmail.com>
2025-03-04 11:03:39 +0530

    Update sum_of_two_num.R

--- sum_of_two_num.R
+++ sum_of_two_num.R
@@ -1,1 +1,11 @@
+add_two_numbers <- function(num1, num2){
++     result <- num1 + num2
++     return(result)
++ }
+> 
+> number1 <- 5
+> number2 <- 6
+> 
+> result <- add_two_numbers(number1,number2)
+> cat("sum = ", result,"\n")
 
